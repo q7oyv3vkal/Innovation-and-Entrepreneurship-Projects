133f92aa4fc653062c4464d561b7f1c44086c099
q7oyv3vkal <105637231+q7oyv3vkal@users.noreply.github.com>
2022-07-06 23:43:47 +0800

    Update SM3.cpp

--- SM3.cpp
+++ SM3.cpp
@@ -1,18 +1,20 @@
 #include "basic_SM3.h"
 
-
-
-int main() {//主函数
+int main() 
+{
 	string str[2];
 	str[0] = "abc";
 	str[1] = "abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd";
-	for (int num = 0; num < 2; num++) {
+	for (int num = 0; num < 2; num++) 
+	{
 		cout << "示例 " + to_string(num + 1) + " ：输入消息为字符串: " + str[num] << endl;
 		cout << endl;
 		string paddingValue = padding(str[num]);
 		cout << "填充后的消息为：" << endl;
-		for (int i = 0; i < paddingValue.size() / 64; i++) {
-			for (int j = 0; j < 8; j++) {
+		for (int i = 0; i < paddingValue.size() / 64; i++) 
+		{
+			for (int j = 0; j < 8; j++)
+			{
 				cout << paddingValue.substr(i * 64 + j * 8, 8) << "  ";
 			}
 			cout << endl;
@@ -20,7 +22,8 @@ int main() {//主函数
 		cout << endl;
 		string result = iteration(paddingValue);
 		cout << "杂凑值：" << endl;
-		for (int i = 0; i < 8; i++) {
+		for (int i = 0; i < 8; i++) 
+		{
 			cout << result.substr(i * 8, 8) << "  ";
 		}
 		cout << endl;
