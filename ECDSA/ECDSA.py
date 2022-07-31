db086d5355cdbfc59a42f89dea64cc0619264da4
q7oyv3vkal <105637231+q7oyv3vkal@users.noreply.github.com>
2022-07-31 11:33:25 +0800

    Create ECDSA.py

---
+++ ECDSA/ECDSA.py
@@ -0,0 +1,42 @@
+#定义散列、ECDSA签名和ECDSA签名验证函数
+from pycoin.ecdsa import generator_secp256k1, sign, verify
+import hashlib, secrets
+
+#哈希函数
+def sha3_256Hash(msg):
+    hashBytes = hashlib.sha3_256(msg.encode("utf8")).digest()
+    return int.from_bytes(hashBytes, byteorder="big")
+  
+#该函数获取文本消息和 256 位 secp256k1 私钥，并计算 ECDSA 签名 {r， s} 并将其作为一对 256 位整数返回。
+def signECDSAsecp256k1(msg, privKey):
+    msgHash = sha3_256Hash(msg)
+    signature = sign(generator_secp256k1, privKey, msgHash)
+    return signature
+  
+#该函数获取文本消息、ECDSA 签名 {r， s} 和 2*256 位 ECDSA 公钥（未压缩），并返回签名是否有效。
+def verifyECDSAsecp256k1(msg, signature, pubKey):
+    msgHash = sha3_256Hash(msg)
+    valid = verify(generator_secp256k1, pubKey, msgHash, signature)
+    return valid
+
+
+# ECDSA签署消息
+msg = "ECDSA签署消息"
+privKey = secrets.randbelow(generator_secp256k1.order())
+signature = signECDSAsecp256k1(msg, privKey)
+print("消息:", msg)
+print("私钥:", hex(privKey))
+print("签名: r=" + hex(signature[0]) + ", s=" + hex(signature[1]))
+
+# ECDSA验证签名
+pubKey = (generator_secp256k1 * privKey).pair()
+valid = verifyECDSAsecp256k1(msg, signature, pubKey)
+print("\n消息:", msg)
+print("私钥: (" + hex(pubKey[0]) + ", " + hex(pubKey[1]) + ")")
+print("签名是否有效?", valid)
+
+# ECDSA验证篡改签名
+msg = "干扰信息"
+valid = verifyECDSAsecp256k1(msg, signature, pubKey)
+print("\n消息:", msg)
+print("签名是否有效(篡改后)?", valid)
